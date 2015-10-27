f2c4bfde7927ab7746fb50d32724bfeaead1f188
Danii <Danii@Samsung-Note>
2015-10-27 19:44:08 -0200

    Cliente

---
+++ Trabalho4Bim/src/main/java/br/univel/cadastro/Cliente.java
@@ -0,0 +1,51 @@
+package br.univel.cadastro;
+
+public class Cliente {
+	
+	private int id;
+	private String nome;
+	private String end;
+	private String tel;
+	private String cidade;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
